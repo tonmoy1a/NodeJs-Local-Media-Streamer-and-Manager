c37fc4a77daf1b1d813c4cfd25d35c99bf5a545e
Tonmoy Das <tonmoy1a@gmail.com>
2022-09-05 20:40:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const fileType = require('file-type');
+const ffmpeg = require('fluent-ffmpeg');
+
+const port = 3000;
+
+app.use(express.static("public"));
+
+//set Base path
+let base_path = "";
+
+const getFiles = async (dir) => {
+    let files = fs.readdirSync(dir);
+
+    let foundDir = [];
+    let foundFiles = [];
+    for (const file of files) {
+        let fullLocation = dir+'/'+file;
+        let fileStatus = fs.statSync(fullLocation);
+
+        if(fileStatus.isDirectory()){
+            foundDir.push(file)
+        }
+
+        if(fileStatus.isFile()){
+            let fileInfo = await fileType.fromFile(fullLocation);
+            foundFiles.push({
+                fileName:file,
+                fileType:fileInfo
+            })
+        }
+        
+    }
+    
+    return {dir:foundDir, files:foundFiles};
+}
+
+const generateThumbnail = (fileName, fnm) => {
+    return new Promise((resolve,reject)=>{
+        console.log(path.join(base_path,fileName));
+
+        console.log(fs.existsSync(path.join(base_path,fileName)))
+        
+        ffmpeg({ source: path.join(base_path,fileName), nolog: true })
+        .takeScreenshots(
+            {
+                timemarks: [ '00:00:10.000' ],
+                filename:fnm+'.png',
+                folder:'cache'
+            }
+        )
+        .on('end', () => {
+            resolve()
+        }).
+        on('error',(err)=>{
+            return reject(new Error(err))
+        })
+        
+    })
+}
+
+app.get('/api', async (req, res) => {
+    let filesAndDir = await getFiles(base_path)
+
+    return res.json(filesAndDir);
+})
+
+app.get('/api/get-file/:folderName', async (req, res) => {
+    let filesAndDir = await getFiles(base_path+'/'+req.params.folderName)
+
+    return res.json(filesAndDir);
+})
+
+app.get('/api/file-stream/:fileName', async (req, res) => {
+    // ffmpeg(base_path+'/'+req.params.fileName)
+    //     .withOutputFormat('mp4')
+    //     .size('600x?')
+    //     .on('end', function(stdout, stdrr){
+    //         console.log('finished')
+    //     })
+    //     .on('error', function(err){
+    //         console.log(err)
+    //     })
+    //     .saveToFile('o.mp4')
+    return res.sendFile(base_path+'/'+req.params.fileName);
+})
+
+app.get('/api/file-thumbnail/:folderPath/:fileName', async (req, res) => {
+    if (!fs.existsSync(__dirname+'/cache/'+req.params.fileName+'.png')) { 
+        await generateThumbnail(decodeURIComponent(req.params.folderPath)+'/'+req.params.fileName, req.params.fileName)
+    }
+
+    return res.sendFile(__dirname+'/cache/'+req.params.fileName+'.png')
+})
+
+app.listen(port,() => {
+    console.log(`app listening at http://localhost:${port}`)
+})
